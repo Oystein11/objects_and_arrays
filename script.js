5bbeb6b46e3486c3ad8f878f587a3151d963e868
Øystein <oystein.koder@gmail.com>
2023-10-12 12:24:32 +0200

    Edits

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 //HTML kobling
-const cat_emoji_list = document.querySelector("#cat_emoji_list_ID");
-const dog_emoji_list = document.querySelector("#dog_emote_list_ID")
+const emoji_list = document.querySelector ("#emoji_list_ID");
+const dog_emoji_list = document.querySelector ("#dog_emoji_list_ID");
 
 //Emoji-ressurs
 const emojis = {
   cat_emojis: ["😺", "😸", "😹", "😻", "😼", "😽", "🙀", "😿", "😾", "🐱"],
   dog_emojis: ["🐶", "🐕", "🦮", "🐩", "🐾", "🐺", "🦊", "🦝", "🐕‍🦺", "🐩‍🦱"],
 };
+console.log (emojis);
 
+//Funksjon
 function display_emojis() {
-    for (a = 0; a < emojis.dog_emojis.length; a++) {
-        let li_element_a = document.createElement("li");
-        li_element_a.innerText = emojis.cat_emojis[a];
-        li_element_a.addEventListener("click", () => {
-        navigator.clipboard.writeText(li_element_a.innerText);
-        console.log(li_element_a.innerText);
+    for (a = 0; a < emojis.cat_emojis.length; a++) {
+        let li_emotes = document.createElement ("li");
+        li_emotes.innerText = emojis.cat_emojis[a];
+        
+        li_emotes.addEventListener ("click", () => {
+        navigator.clipboard.writeText(li_emotes.innerText);
+        alert ("Du la  " + li_emotes.innerText + "-emojien til utklippstavlen din.");
+        console.log ("Du la  " + li_emotes.innerText + "emojien til utklippstavlen din.");
     });
-    cat_emoji_list.appendChild(li_element_a);
+    emoji_list.appendChild(li_emotes);
   };
 };
