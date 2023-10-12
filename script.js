9dc1555b6e62bf23facb9b3526af877ee99f6572
Øystein <oystein.koder@gmail.com>
2023-10-12 09:34:25 +0200

    Slight change

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+//HTML kobling
+const displayer = document.querySelector ("pictureID");
+console.log (displayer);
+
+const cat_button = document.querySelector ("#catID");
+console.log (cat_button);
+
+const all_button = document.querySelector ("#allID");
+console.log (all_button);
+
+// Emoji-ressurs
 const emojis = {
     catEmojis: ["😺", "😸", "😹", "😻", "😼", "😽", "🙀", "😿", "😾", "🐱"],
     dogEmojis: ["🐶", "🐕", "🦮", "🐩", "🐾", "🐺", "🦊", "🦝", "🐕‍🦺", "🐩‍🦱"],
     };
-    console.log (emojis)
+console.log (emojis);
+
+//Display-variabel
+let display = 0;
+
+//Cat-button
+function generate_cat () {
+ display > emojis.catEmojis, 1++;
+};
+
+//All-button
+function generate_all () {
+
+};
